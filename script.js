7f019a8997c3be821fe796da38a20d03fc81668a
TToMaT0 <141110446+TToMaT0@users.noreply.github.com>
2023-09-13 12:26:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+    const elements = document.querySelectorAll('.fade-in-element');
+    
+    elements.forEach((element) => {
+        if (isElementInViewport(element)) {
+            element.style.opacity = '1';
+            element.style.transform = 'translateY(0)';
+        }
+    });
+}
+
+// Listen for the scroll event and invoke the handleScroll function
+window.addEventListener('scroll', handleScroll);
+
+// Initial check to see if any elements are in the viewport on page load
+handleScroll();
